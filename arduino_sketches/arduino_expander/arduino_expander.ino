fc4bf882da980518b5bf59ceb9e14b908c44225e
Stroe Andrei Catalin <catalin.stroe@conceptapps.ro>
2023-02-23 15:11:43 +0200

    Fixed bitmask

--- arduino_sketches/arduino_expander/arduino_expander.ino
+++ arduino_sketches/arduino_expander/arduino_expander.ino
@@ -13,9 +13,14 @@
 #define CMD_RESTORE_OUTPUTS 0x06
 #define CMD_ADC_SOURCE 0x07
 
+#define DEBUG
+
 uint8_t buffer[14] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
 void setup() {
+#ifdef DEBUG
+  Serial.begin(115200);
+#endif
   wdt_disable();
   for (uint8_t i = 0; i < 6; i++) {
     uint8_t pin = (i >= 4) ? i + 2 : i;
@@ -50,7 +55,7 @@ void onRequest() {
   Wire.write(buffer, 14);
 }
 
-void onReceive(uint8_t numBytes) {
+void onReceive(int numBytes) {
   switch (Wire.read()) {
     case CMD_READ:
       readData();
@@ -77,35 +82,94 @@ void onReceive(uint8_t numBytes) {
 }
 
 void setupPins(uint8_t data1, uint8_t data0) {
-  for (uint8_t i = 0; i < 8; i++)
-    pinMode(i, data0 & (1 << i));
-  for (uint8_t i = 0; i < 6; i++)
-    pinMode(i + 8, data1 & (1 << i));
+  for (uint8_t i = 0; i < 8; i++) {
+    bool mode = data0 & (1 << i);
+#ifdef DEBUG
+    Serial.print("Setting pin: ");
+    Serial.print(i);
+    Serial.print(" as: ");
+    Serial.println((mode) ? "OUTPUT" : "INPUT");
+#endif
+    pinMode(i, mode);
+  }
+  for (uint8_t i = 0; i < 6; i++) {
+    bool mode = data1 & (1 << i);
+#ifdef DEBUG
+    Serial.print("Setting pin: ");
+    Serial.print(i + 8);
+    Serial.print(" as: ");
+    Serial.println((mode) ? "OUTPUT" : "INPUT");
+#endif
+    pinMode(i + 8, mode);
+  }
 }
 
 void setupInputPullupPins(uint8_t data1, uint8_t data0) {
-  for (uint8_t i = 0; i < 8; i++)
-    if (data0 & (1 << i)) pinMode(i, INPUT_PULLUP);
-  for (uint8_t i = 0; i < 6; i++)
-    if (data1 & (1 << i)) pinMode(i + 8, INPUT_PULLUP);
+  for (uint8_t i = 0; i < 8; i++) {
+    if (data0 & (1 << i)) {
+#ifdef DEBUG
+      Serial.print("Setting pin: ");
+      Serial.print(i);
+      Serial.println(" as: INPUT_PULLUP ");
+#endif
+      pinMode(i, INPUT_PULLUP);
+    }
+  }
+  for (uint8_t i = 0; i < 6; i++) {
+    if (data1 & (1 << i)) {
+#ifdef DEBUG
+      Serial.print("Setting pin: ");
+      Serial.print(i + 8);
+      Serial.println(" as: INPUT_PULLUP ");
+      pinMode(i + 8, INPUT_PULLUP);
+#endif
+    }
+  }
 }
 
 void restoreOutputs(uint8_t data1, uint8_t data0) {
-  for (uint8_t i = 0; i < 8; i++)
-    if (data0 & (1 << i)) digitalWrite(i, HIGH);
-  for (uint8_t i = 0; i < 6; i++)
-    if (data1 & (1 << i)) digitalWrite(i + 8, HIGH);
+  Serial.println(data0, BIN);
+  Serial.println(data1, BIN);
+  for (uint8_t i = 0; i < 8; i++) {
+    if (data0 & (1 << i)) {
+#ifdef DEBUG
+      Serial.print("Restoring pin: ");
+      Serial.print(i);
+      Serial.println(" as: HIGH ");
+#endif
+      digitalWrite(i, HIGH);
+    }
+  }
+  for (uint8_t i = 0; i < 6; i++) {
+    if (data1 & (1 << i)) {
+#ifdef DEBUG
+      Serial.print("Restoring pin: ");
+      Serial.print(i + 8);
+      Serial.println(" as: HIGH ");
+#endif
+      digitalWrite(i + 8, HIGH);
+    }
+  }
 }
 
 void setAdcSource(uint8_t data1, uint8_t data0) {
   switch (data1) {
     case 0:
+#ifdef DEBUG
+      Serial.println("Setting analog reference: DEFAULT");
+#endif
       analogReference(DEFAULT);
       break;
     case 1:
+#ifdef DEBUG
+      Serial.println("Setting analog reference: INTERNAL");
+#endif
       analogReference(INTERNAL);
       break;
     case 2:
+#ifdef DEBUG
+      Serial.println("Setting analog reference: EXTERNAL");
+#endif
       analogReference(EXTERNAL);
       break;
   }
