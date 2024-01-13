b7be1aae4286d8d940e1678b45c05ad23c469324
Stroe Andrei Catalin <catalin.stroe@conceptapps.ro>
2024-01-13 19:29:43 +0200

    fix cc1101 module number

--- components/cc1101/cc1101.h
+++ components/cc1101/cc1101.h
@@ -12,16 +12,14 @@ class CC1101 : public Component {
 public:
   void dump_config() override;
   void setup() override;
-  void set_spi_pins(InternalGPIOPin *clk_pin, InternalGPIOPin *miso_pin,
-                    InternalGPIOPin *mosi_pin, InternalGPIOPin *cs_pin) {
+  void set_spi_pins(InternalGPIOPin *clk_pin, InternalGPIOPin *miso_pin, InternalGPIOPin *mosi_pin,
+                    InternalGPIOPin *cs_pin) {
     this->clk_pin_ = clk_pin->get_pin();
     this->miso_pin_ = miso_pin->get_pin();
     this->mosi_pin_ = mosi_pin->get_pin();
     this->cs_pin_ = cs_pin->get_pin();
   };
-  void set_d0_pin(InternalGPIOPin *d0_pin) {
-    this->d0_pin_ = d0_pin->get_pin();
-  };
+  void set_d0_pin(InternalGPIOPin *d0_pin) { this->d0_pin_ = d0_pin->get_pin(); };
   void set_bandwidth(uint32_t bandwidth) { this->bandwidth_ = bandwidth; };
   void set_frequency(uint32_t frequency) { this->frequency_ = frequency; };
 
@@ -31,7 +29,6 @@ private:
   uint8_t mosi_pin_;
   uint8_t cs_pin_;
   uint8_t d0_pin_;
-  uint8_t module_number_{1};
   uint32_t bandwidth_;
   uint32_t frequency_;
 };
