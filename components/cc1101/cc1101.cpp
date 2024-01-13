2470f18c531eb296561afa3ee7e97551112f19cc
Stroe Andrei Catalin <catalin.stroe@conceptapps.ro>
2024-01-13 19:35:04 +0200

    fix cc1101 module number

--- components/cc1101/cc1101.cpp
+++ components/cc1101/cc1101.cpp
@@ -12,14 +12,13 @@ void CC1101::dump_config() {
                 this->d0_pin_);
   ESP_LOGCONFIG(TAG, "  Frequency:%u", this->frequency_);
   ESP_LOGCONFIG(TAG, "  Bandwidth:%u", this->bandwidth_);
-  ESP_LOGCONFIG(TAG, "  Module number:%f", this->module_number_);
 }
 
 void CC1101::setup() {
   pinMode(this->d0_pin_, INPUT);
   ELECHOUSE_cc1101.addSpiPin(this->clk_pin_, this->miso_pin_, this->mosi_pin_,
-                             this->cs_pin_, this->module_number_);
-  ELECHOUSE_cc1101.setModul(this->module_number_);
+                             this->cs_pin_, 1);
+  ELECHOUSE_cc1101.setModul(1);
   ELECHOUSE_cc1101.Init();
   ELECHOUSE_cc1101.setRxBW(this->bandwidth_ / 1000);
   ELECHOUSE_cc1101.setMHZ(this->frequency_ / 1000000);
